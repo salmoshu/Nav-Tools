b927bcfd94178f9204c7995e97d27e14f1e5b562
winchell.hu <winchell.hu@outlook.com>
2025-09-16 17:38:40 +0800

    feat：去掉example相关内容，仅在README.md中进行介绍

--- src/stores/demo1.ts
+++ src/stores/demo1.ts
@@ -2,7 +2,20 @@ import { defineStore } from "pinia"
 import { appConfig } from "@/settings/config"
 
 // 获取demo1Props配置
-const props = appConfig.example.demo1.props
+// const props = appConfig.example.demo1.props
+const props = {
+  status: {
+    str: 'Nav-Tools',
+    num: 2,
+  },
+  config: {
+    'Shirt': 5,
+    'Wool Sweater': 20,
+    'Pants': 10,
+    'High Hells': 10,
+    'Socks': 20,
+  },
+}
 
 export const useDemo1Store = defineStore("demo1", {
   state: () => {
